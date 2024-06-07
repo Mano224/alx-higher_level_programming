58a329806805422ff9872da672b88955331c819f
Mano224 <mano@example.com>
2024-06-08 00:36:52 +0300

    hh

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 '''Module for MyList class.'''
 
-class Mylist(list):
+
+class MyList(list):
     '''Custom MyList class.'''
     def print_sorted(self):
         '''Method for printing sorted list.'''
