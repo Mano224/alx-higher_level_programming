0d40bdea05e942cfaede15eab80284cbf163e109
Mano224 <mano@example.com>
2024-06-09 00:40:05 +0300

    hh

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""defining read_file function"""
+"""defining write_file with two arguments"""
 
-def read_file(filename="", text=""):
+def write_file(filename="", text=""):
     """read filename with utf-8"""
     with open(filename, "w" , encoding='utf-8') as f:
         return f.write(text)
