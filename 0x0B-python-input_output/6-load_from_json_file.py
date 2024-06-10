a06fd91f556ab08bfc146e420022f6431ace08b1
Mano224 <mano@example.com>
2024-06-10 15:08:48 +0300

    hh

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,6 @@
 import json
 
 def load_from_json_file(filename):
-    """ """
+    """creat an object from json file """
     with open(filename, 'r', encoding="utf-8") as file:
        return json.load(file)
