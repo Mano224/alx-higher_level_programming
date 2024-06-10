a5ef919ee34041c0cdd900e9ff4f800e00728eca
Mano224 <mano@example.com>
2024-06-10 14:54:03 +0300

    hh

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""difining save_to_josn_file function"""
+
+import json 
+
+def save_to_json_file(my_obj, filename):
+    """write an object to text file"""
+    with open(filename, 'w', encoding="utf_8") as file:
+        json.dump(my_obj, file)
+
