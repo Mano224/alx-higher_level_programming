f988a2ee97841bc241b4c0c54481ac26f17d7a43
Mano224 <mano@example.com>
2024-06-12 22:32:22 +0300

    hh

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''contains the class "student"'''
+"""contains the class "student"""
 
 class student:
-    '''Representation of a student'''
+    """Representation of a student"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -10,4 +10,3 @@ class student:
     def to_json(self):
         '''return a dictionary representaion of a student instance'''
         return self.__dict__
-
