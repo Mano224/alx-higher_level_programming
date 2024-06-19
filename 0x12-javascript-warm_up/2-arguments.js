23e88e323ee2e655f56bcc40bbce63b2df3ef376
Mano224 <mano@example.com>
2024-06-19 11:08:31 -0700

    hh

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 /* learning how to pass arguments*/
 if (process.argv.length === 2){
         console.log('No argument');
@@ -8,19 +7,6 @@ else if (process.argv.length === 3){
         console.log('Argument found');
 }
 else {
-        console.log('Arguments found');
+console.log('Arguments found');
 }
-=======
-process = require(args)
-if (args.length === 0){
-	console.log('No argument');
-}
-else if (args.length === 1){
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
-}
-args = process.argv
->>>>>>> 240a82c820c6623aef953ef2de010de5f35520e1
 
