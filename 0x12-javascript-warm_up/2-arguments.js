e9e359ee801993de97db119d62ab68cdd3f55c71
Mano224 <mano@example.com>
2024-06-19 18:34:14 +0300

    hh

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function find(args){
+	if (args.length === 0){
+		console.log('No argument');
+	}
+	else if (args === 1){
+		console.log('Argument found');
+	} 
+	else {
+		console.log('Argument found');
+	}
+}
