11a275390daef4156ccf2697642d511012a3a112
Mano224 <mano@example.com>
2024-06-19 18:36:33 +0300

    hh

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
-function find(args){
-	if (args.length === 0){
-		console.log('No argument');
-	}
-	else if (args === 1){
-		console.log('Argument found');
-	} 
-	else {
-		console.log('Argument found');
-	}
+function printMessage(args) {
+    if (args.length === 0) {
+        console.log("No argument");
+    } else if (args.length === 1) {
+        console.log("Argument found");
+    } else {
+        console.log("Arguments found");
+    }
 }
+
+// Example usage
+const args = process.argv.slice(2);
+printMessage(args);
+
