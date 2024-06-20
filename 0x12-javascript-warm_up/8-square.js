65e35ce902448172d125cfb8e39c32f6bfd2783a
Mano224 <mano@example.com>
2024-06-20 09:44:46 -0700

    hh

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+x = parseInt(process.argv[2]);
+if (!isNaN(x)){
+	for ( let i = 0; i < x; i++){
+		for ( let j = 0; j < x; j++){
+			console.log(x):
+		}
+	}
+}
+else {
+	console.log('Missing size');
+}
