2d0f155a01edbd9d4e54c87aaa2437b0a9926854
Mano224 <mano@example.com>
2024-06-20 07:36:02 -0700

    hh

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for ( let i = 0; i < args; i++){
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < args; i++){
 	console.log(args[i]);
 }
