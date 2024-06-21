8eb962b967ff9601b1466335d16e602e47a6222c
Mano224 <mano@example.com>
2024-06-21 07:47:20 -0700

    hh

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-function add(a, b){
-return a + b;
+
+function add (a, b) {
+  return a + b;
 }
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-if (!isNaN(a) && !isNaN(b)){
-	console.log(add(a, b);
-}
-else {
-	console.log('NaN');
+const x = parseInt(process.argv[2]);
+const y = parseInt(process.argv[3]);
+if (!isNaN(x) && !isNaN(y)) {
+  console.log(add(x, y));
+} else {
+  console.log('NaN');
 }
 
