abaca064888485d2d048cbd1dc736003cc29762c
Mano224 <mano@example.com>
2024-06-22 16:09:19 +0300

    hh

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const Rectangle = require('./0-rectangle');
+class Rectangle {
+    // Define properties and methods here
+}
 
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
