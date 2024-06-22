5f3188b4ed106165a4f41de65b43e1a0b2f2e546
Mano224 <mano@example.com>
2024-06-22 22:47:37 +0300

    hh

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,12 @@
+
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement){
-	let counter = 0;
-	for (let i = 0; i < list.lenght; i++){
-		if (list[i] === searchElement) {
-			counter++;
-		}
-	}
-	return counter;
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
 };
+
