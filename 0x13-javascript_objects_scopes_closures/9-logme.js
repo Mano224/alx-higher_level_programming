cebb9e637d5b73b10fa0cbe15d9c0541d4fe04a4
Mano224 <mano@example.com>
2024-06-23 00:15:14 +0300

    hh

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-exports.logMe = function (item){
-	let count = 0;
-	console.log(`${count++}: ${item});
-	};
+let count = 0;
+exports.logMe = function (item) { console.log(`${count++}: ${item}`); 
+};
