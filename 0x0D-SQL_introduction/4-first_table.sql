00ca5b069b3e9859cf4136768926c85c4947bf19
Mano224 <mano@example.com>
2024-06-24 23:00:36 +0300

    hh

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Create a first_table in database
 -- Exceute: cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-CREATE TABLES IF NOT EXISTS first_table(id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
