3a4ac0a31a21faef9af801af344de88367b260aa
Mano224 <mano@example.com>
2024-09-21 17:23:18 +0300

    f

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,18 +4,19 @@
 
 import sys
 import MySQLdb
+if __name__ == '__main__':
+    conn = MuSQLdb.connect(user = sys.argv[1],
+            password = sys.argv[2],
+            db = sys.argv[3])
+    cursor = conn.cursor()
 
-conn = MuSQLdb.connect(user = sys.argv[1],
-        password = sys.argv[2],
-        db = sys.argv[3])
-cursor = conn.cursor()
+    query = "SELECT cities.id, cities.name, states.name\
+                    FROM cities LEFT JOIN states\
+                    ON states.id = cities.state_id\
+                    ORDER BY cities.id ASC"
+    cursor.execute(query)
+    row = cursor.fetchall()
+    for r in row:
+        print(r)
 
-query = "SELECT * FROM cities ORDER BY cities.id ASC"
-cursor.execute(query)
-row = cursor.fetchall()
-for r in row:
-    print(r)
-
-cursor.close()
-
-
+    cursor.close()
