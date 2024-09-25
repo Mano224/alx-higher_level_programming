e59d1ac3bc0278a28ea741e03a44d642bb446981
Mano224 <mano@example.com>
2024-09-25 22:53:58 +0300

    d

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
 
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        print(dic(response.header).get("X-Request-Id"))
+        print(dict(response.header).get("X-Request-Id"))
