1ec97c617561f409c645d003dba60aa35cfd233f
Mano224 <mano@example.com>
2024-09-25 22:42:34 +0300

    d

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("\t- type: {}".format(type(body)))
-        print("\t- content: b{}".format(body))
+        print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
